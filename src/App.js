a0539f9f618a9515309b7522bbd0c81a1fe3fcbb
Megan Miller <meganmiller@Megans-MacBook-Pro.local>
2017-09-25 13:44:59 -0600

    Helpful hints added.

--- src/App.js
+++ src/App.js
@@ -2,49 +2,55 @@ import React, { Component } from "react";
 
 import "./App.css";
 
+import { connect } from 'react-redux';
+import { increment, decrement } from './ducks/counter';
+
 export class App extends Component {
 	render() {
+
+		let { currentValue, increment, decrement } = this.props;
+
 		return (
 			<div className="app">
 				<section className="counter">
-					<h1 className="counter__current-value">{ 0 }</h1>
+					<h1 className="counter__current-value">{currentValue}</h1>
 					<div className="counter__button-wrapper">
 						<button
 							className="counter__button increment-one"
-							onClick={ () => null }
+							onClick={() => increment(1)}
 						>
 							+1
 						</button>
 						<button
 							className="counter__button increment-five"
-							onClick={ () => null }
+							onClick={() => increment(5)}
 						>
 							+5
 						</button>
 						<button
 							className="counter__button decrement-one"
-							onClick={ () => null }
+							onClick={() => decrement(1)}
 						>
 							-1
 						</button>
 						<button
 							className="counter__button decrement-five"
-							onClick={ () => null }
+							onClick={() => decrement(5)}
 						>
 							-5
 						</button>
 						<br />
 						<button
 							className="counter__button undo"
-							disabled={ true }
-							onClick={ () => null }
+							disabled={true}
+							onClick={() => null}
 						>
 							Undo
 						</button>
 						<button
 							className="counter__button redo"
-							disabled={ true }
-							onClick={ () => null }
+							disabled={true}
+							onClick={() => null}
 						>
 							Redo
 						</button>
@@ -52,7 +58,7 @@ export class App extends Component {
 				</section>
 				<section className="state">
 					<pre>
-						{ JSON.stringify( this.props, null, 2 ) }
+						{JSON.stringify(this.props, null, 2)}
 					</pre>
 				</section>
 			</div>
@@ -60,4 +66,15 @@ export class App extends Component {
 	}
 }
 
-export default App;
+function mapStateToProps(state) {
+	return state;
+}
+
+let outActions = {
+	increment,
+	decrement
+}
+
+let connected = connect(mapStateToProps, outActions)
+
+export default connected(App);
--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,2 +1,41 @@
 // change this to true to see test results on the black diamond section.
 export const BLACK_DIAMOND = false;
+
+
+//CONSTANTS
+const INCREMENT = "INCREMENT"
+const DECREMENT = "DECREMENT"
+
+//ACTION BUILDERS
+export function increment(num) {
+    return {
+        type: INCREMENT,
+        payload: num
+    }
+}
+
+export function decrement(num) {
+    return {
+        type: DECREMENT,
+        payload: num
+    }
+}
+
+//REDUCER
+
+let initialState = {
+    currentValue: 0
+}
+
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+        case INCREMENT:
+            let newNum = state.currentValue + action.payload;
+            return Object.assign({}, state, { currentValue: newNum })
+        case DECREMENT:
+            let decreNum = state.currentValue - action.payload;
+            return Object.assign({}, state, {currentValue: decreNum})
+        default:
+            return state;
+    }
+}
